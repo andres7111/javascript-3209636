8ac088c7c42bd7426541b41bf30e0b47a2a6d56d
andreschavarriaga0907 <valdesandres859@gmail.com>
2025-11-21 15:29:23 -0500

    Avance 21/11/25.

---
+++ 2-base-datos-comic/bd.js
@@ -0,0 +1,117 @@
+let comic={
+  "nombrecomic": "adrian y las cronicas del imperio",
+  "sinopsis": "arquologo viajero del timepo",
+  "year": 2025,
+  "autores": ["stiven","hillary","kate","andres"],
+  "persoanjes": [
+
+    {"nombre": "adrian", 
+  "imagen": ""
+
+},
+ {"nombre": "monje", 
+      "image": "./img/pp.png"
+
+},
+ {"nombre": "ninov", 
+      "image": "./imagenes/marvel-captain-america-png-image-purepng-transparent-30.png"
+
+},
+ {"nombre": "platon", 
+  "imagen": "./imagenes/platon"
+
+},
+ {"nombre": "aristoteles", 
+  "imagen": "./imagenes/aristoteles"
+
+},
+ {"nombre": "socrates", 
+  "imagen": "./imagenes/socrates"
+
+},
+ {"nombre": "senador", 
+  "imagen": "./imagnes/senador"
+
+},
+
+
+
+
+
+
+
+  ],
+  "genero": "ficiion",
+  "portada": "./imagenes/portada.jpg",
+    "escenas": [
+    {
+        "id":1,
+      "nombre": "Escena 1",
+      "personajes": "Adrian",
+      "image": "./imagen/ Adrian"
+    },
+    
+    {
+         "id":2,
+      "nombre": "Escena 2",
+      "personajes": "monje",
+      "image": "./imagen/Monje"
+      
+    },
+    {
+        "id":3,
+      "nombre": "Escena 3",
+      "personajes": "Senador",
+      "image": "./imagen/Senador"
+    },
+
+
+     {
+        "id":4,
+      "nombre": "Escena 4",
+      "personajes": "Aristoteles",
+      "image": "./imagen/Aristoteles"
+    },
+
+
+     {
+        "id":5,
+      "nombre": "Escena 5",
+      "personajes": "Socrates",
+      "image": "./imagen/Socrates"
+    },
+
+
+     {
+        "id":6,
+      "nombre": "Escena 6",
+      "personajes": "Platón",
+      "image": "./imagen/Platón"
+    },
+
+
+
+
+     {
+        "id":7,
+      "nombre": "Escena 7",
+      "personajes": "Ninove",
+      "image": "./imagen/Ninove"
+    },
+
+
+     {
+        "id":8,
+      "nombre": "Escena 8",
+      "personajes": "Dan Jukes",
+      "image": "./imagenes/marvel-captain-america-png-image-purepng-transparent-30.png"
+    },
+
+
+
+
+
+
+  
+  ]
+}
