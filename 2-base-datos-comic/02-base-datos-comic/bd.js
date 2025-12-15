8870b5ec88e6a2fdcc96e1a5f267d65ffcdcab94
andreschavarriaga0907 <valdesandres859@gmail.com>
2025-12-15 14:17:24 -0500

    Terminado.

---
+++ 2-base-datos-comic/02-base-datos-comic/bd.js
@@ -0,0 +1,139 @@
+export let comic={
+  "nombrecomic": "ADRIAN Y LAS CRONICAS DEL IMPERIO",
+  "sinopsis": "Arqueologo Viajero Del Timepo",
+  "year": 2025,
+  "autores": ["STIVEN","HILLARY","KATE","ANDRES"],
+  "personajes": [
+
+    { "id":1,
+      "nombre": "Adrian", 
+      "image": "./img/adrian.png",
+      "descripcion":"arqueologo "
+},
+ { "id":2,
+  "nombre": "Monje", 
+      "image": "./img/monje (2).png",
+  "descripcion":"Arqueologo "
+},
+ {"id":3,
+  "nombre": "NINOVE", 
+      "image": "./img/ninove.png",
+  "descripcion":"Arqueologo "
+},
+ { "id":4,
+  "nombre": "PLATON", 
+      "image": "./img/platon.png",
+  "descripcion":"rqueologo "
+},
+ { "id":5,
+  "nombre": "ARISTOTELES", 
+      "image": "./img/aristoteles.png",
+  "descripcion":"Arqueologo "
+},
+ {"id":6,
+  "nombre": "SOCRATES", 
+      "image": "./img/socrates.png",
+  "descripcion":"Arqueologo "
+},
+ {"id":7,
+  "nombre": "SENADOR", 
+      "image": "./img/senador.png",
+  "descripcion":"Arqueologo "
+}, ],
+  "genero": "fICCION",
+  "portada": "./img/portada.jpg",
+    "escenas": [
+    {
+        "id":1,
+      "nombre": "FORO ROMANO",
+      "personajes": "ADRIAN",
+      "image": "./img/foro romano.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+       "ver": "Mas capitulos",
+    },
+    
+    {
+         "id":2,
+      "nombre": "TEMPLO",
+      "personajes": "MONJE",
+      "image": "./img/templo.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "Mas capitulos",
+    },
+    {
+        "id":3,
+      "nombre": "PANTEON",
+      "personajes": "Platon",
+      "image": "./img/panteon.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+     "ver": "Mas capitulos",  
+      },
+
+
+     {
+        "id":4,
+      "nombre": "CIRCO",
+      "personajes": "NINOVE",
+      "image": "./img/circo.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "Mas capitulos",
+    },
+
+
+     {
+        "id":5,
+      "nombre": "TEATRO",
+      "personajes": "ARISTOTELES",
+      "image": "./img/teatro.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "Mas capitulos",
+    },
+
+
+     {
+        "id":6,
+      "nombre": "TERMNAS",
+      "personajes": "SOCRATES",
+      "image": "./img/termas.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "Mas capitulos",
+    },
+
+
+
+
+     {
+        "id":7,
+      "nombre": "BIBLIOTECA",
+      "personajes": "SENADOR",
+      "image": "./img/biblioteca.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "mas capitulos",
+    },
+
+
+     {
+        "id":8,
+      "nombre": "GRECIA",
+      "personajes": "Dan Jukes",
+      "image": "./img/templo.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "mas capitulos",
+    },
+
+
+
+
+
+
+  
+  ]
+}
